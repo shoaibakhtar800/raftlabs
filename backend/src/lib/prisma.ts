ca011a563e1b27887fe6eaee75742034222acde3
shoaibakhtar800 <cs08018akp@gmail.com>
2026-02-07 10:07:52 +0530

    Refactor: Update import statement in prisma.ts to include file extension for consistency

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "../../generated/prisma/index.js";
 import pg from "pg";
 
 const connectionString = process.env.DATABASE_URL;
